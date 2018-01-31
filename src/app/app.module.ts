d2427f26871ee5b27590b1b6487ab57f789205c8
Derick Beckwith <wbeckwith@enclarapharmacia.com>
2018-01-31 11:45:00 -0500

    Add Hero List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule
