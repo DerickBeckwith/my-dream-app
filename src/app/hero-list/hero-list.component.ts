9fcccf728e7fb376e18974b65eb73cdb2649d0b8
Derick Beckwith <wbeckwith@enclarapharmacia.com>
2018-01-31 14:13:21 -0500

    Add and display array of colors

--- src/app/hero-list/hero-list.component.ts
+++ src/app/hero-list/hero-list.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HeroListComponent implements OnInit {
 
   selectedHero = { id: 1, name: 'Ms Tafling', power: 'speed' };
+  colors = [ 'red', 'yellow', 'green' ];
+
   constructor() { }
 
   ngOnInit() {
